cb5a67263ee6a6fdae68a7ec7c38b8c30b6312b3
radekaadek <radekaadek@gmail.com>
2024-10-17 14:46:18 +0200

    end of lesson

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 import rasterio
 import os
 import pandas as pd
+import numpy as np
 
 def find_file(base_path: str, file_ending: str) -> str:
     for root, _, files in os.walk(base_path):
@@ -24,7 +25,6 @@ def process_feature(feature_name: str, area_polygon: gpd.GeoDataFrame) -> pd.Dat
     f2 = find_file(lwowecki_name, feature_name)
     feature = read_and_concat_files([f1, f2])
     # crop feature to area of interest
-    # return feature.clip(area_polygon)
     clipped = feature.clip(area_polygon)
     if clipped is None:
         return gpd.GeoDataFrame()
@@ -42,14 +42,40 @@ lwowecki_name = '02_GML/0212_GML'
 rivers = 'SWRS_L.xml'
 buildings = 'BUBD_A.xml'
 
+woda_powierzchnia = 'PTWP_A.xml'
+zabudowa = 'PTZB_A.xml'
+teren_lesny = 'PTLZ_A.xml'
+roslinnosc_krzewiasta = 'PTRK_A.xml'
+uprawa_trwala = 'PTUT_A.xml'
+roslinnosc_trawiasta = 'PTTR_A.xml'
+teren_pod_drogami = 'PTKM_A.xml'
+grunt_nieuzytkowy = 'PTGN_A.xml'
+plac = 'PTPL_A.xml'
+skladowisko_odpadow = 'PTSO_A.xml'
+wyrobisko = 'PTWZ_A.xml'
+pozostale = 'PTNZ_A.xml'
+
+
 area_polygon = gpd.read_file('swieradow_buffer/swieradow_buffer.shp')
 
 rivers_frame = process_feature(rivers, area_polygon)
 buildings_frame = process_feature(buildings, area_polygon)
+woda_powierzchnia_frame = process_feature(woda_powierzchnia, area_polygon)
+zabudowa_frame = process_feature(zabudowa, area_polygon)
+teren_lesny_frame = process_feature(teren_lesny, area_polygon)
+roslinnosc_krzewiasta_frame = process_feature(roslinnosc_krzewiasta, area_polygon)
+teren_pod_drogami_frame = process_feature(teren_pod_drogami, area_polygon)
+grunt_nieuzytkowy_frame = process_feature(grunt_nieuzytkowy, area_polygon)
+plac_frame = process_feature(plac, area_polygon)
+skladowisko_odpadow_frame = process_feature(skladowisko_odpadow, area_polygon)
+wyrobisko_frame = process_feature(wyrobisko, area_polygon)
 
-print(rivers_frame.head())
-print(buildings_frame.head())
+
+# load the dem
+result = rasterio.open('temp/xd.tif')
+band = result.read(1)
+band = np.zeros(band.shape)
 
 # save buildings to buildings.gpkg
-buildings_frame.to_file('buildings.gpkg')
+# buildings_frame.to_file('buildings.gpkg')
 
