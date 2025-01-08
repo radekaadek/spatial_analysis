edcbb2de99c2a360d6d8b744ef58eee21ff6d95d
radekaadek <radekaadek@gmail.com>
2025-01-08 11:08:39 +0100

    generate pharmacies

--- 2/main.py
+++ 2/main.py
@@ -8,6 +8,8 @@ border = gpd.read_file("projekt_2_granice/strefa wielkomiejska_bufor_200.shp").t
 # check if osm_data folder exists
 osm_data_folder = "osm_data"
 pathlib.Path(osm_data_folder).mkdir(parents=True, exist_ok=True)
+generated_data_folder = "generated_data"
+pathlib.Path(generated_data_folder).mkdir(parents=True, exist_ok=True)
 
 # check if lodzkie-latest.osm.pbf exists
 if not pathlib.Path("lodzkie-latest.osm.pbf").exists():
@@ -30,11 +32,15 @@ for idx, layer in gpd.list_layers("lodzkie-latest.osm.pbf").iterrows():
     cut = df.clip(border).to_crs("EPSG:2180")
 
 # read points and get shops
-points = gpd.read_file("osm_data/points.gpkg")
+points = gpd.read_file(f"{osm_data_folder}/points.gpkg")
+# save unique other_tags to csv
+points["other_tags"].drop_duplicates().to_csv(f"{generated_data_folder}/other_tags.csv", index=False)
 # shops are points that contain the string \"shop\"=> in other_tags
 # change type to string
 points["other_tags"] = points["other_tags"].astype(str)
 shops = points[points["other_tags"].str.contains('"shop"=>')]
 # save as shops.gpkg
-shops.to_file(f"{osm_data_folder}/shops.gpkg")
+shops.to_file(f"{generated_data_folder}/shops.gpkg")
+pharmacies = points[points["other_tags"].str.contains('"pharmacy"')]
+pharmacies.to_file(f"{generated_data_folder}/pharmacies.gpkg")
 
